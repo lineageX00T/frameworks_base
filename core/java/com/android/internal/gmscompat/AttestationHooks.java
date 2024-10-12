c0edacc8a52c3317947722795123338af1a8b54c
Meisie Widuri <ewprjkt@proton.me>
2024-10-12 13:37:41 +0700

    AttestationHooks: Update fingerprint

--- core/java/com/android/internal/gmscompat/AttestationHooks.java
+++ core/java/com/android/internal/gmscompat/AttestationHooks.java
@@ -70,10 +70,10 @@ public final class AttestationHooks {
 
     private static void spoofBuildGms() {
         // Alter model name and fingerprint to avoid hardware attestation enforcement
-        setBuildField("DEVICE", "sailfish");
-        setBuildField("FINGERPRINT", "google/sailfish/sailfish:8.1.0/OPM1.171019.011/4448085:user/release-keys");
-        setBuildField("MODEL", "Pixel");
-        setBuildField("PRODUCT", "sailfish");
+        setBuildField("DEVICE", "oriole");
+        setBuildField("FINGERPRINT", "google/oriole_beta/oriole:15/AP41.240823.009/12329489:user/release-keys");
+        setBuildField("MODEL", "Pixel 6");
+        setBuildField("PRODUCT", "oriole_beta");
         setVersionField("DEVICE_INITIAL_SDK_INT", Build.VERSION_CODES.N);
     }
 
